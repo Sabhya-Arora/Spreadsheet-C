78250d3ddcfa65e76d3cf2de547e711e26a5dee5
Awesomium10 <adityanarware123@gmail.com>
2025-02-17 13:08:19 +0530

    input parser now has a validity checker that can be called in main before barsing

--- input_parser.c
+++ input_parser.c
@@ -2,28 +2,219 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
+#include "Decl.h"
+
+//Validity checker
+
+void to_uppercase(char *str){
+    while(*str){
+        *str = toupper(*str);
+        str++;
+    }
+}
+
+void remove_spaces(char* s){
+    char* d = s;
+    do{
+        while(*d == ' '){
+            ++d;
+        }
+    } while(*s++ = *d++);
+}
+
+int base26_digits(int x){
+    if (x == 0) return 1; 
+    return (int)(log10(x) / log10(26)) + 1;
+}
+
+int base10_digits(int x){
+    if (x==0) return 1;
+    return (int)(log10(x)) + 1;
+}
+
+int is_valid_cell(char *cell, int R, int C){
+    int max_alpha = base26_digits(R);
+    int max_num = base10_digits(C);
+    int max_len = max_alpha + max_num + 1;
+    if(cell == NULL || *cell == '\0') return 0;
+    if(strlen(cell) > max_len) return 0;
+    int alpha_count=0;
+    int num_count=0;
+    while(*cell && isalpha(*cell)){
+        alpha_count++;
+        cell++;
+    }
+    while(*cell && isdigit(*cell)){
+        num_count++;
+        cell++;
+    }
+    if(alpha_count==0 || alpha_count>max_alpha) return 0;
+    if(num_count==0 || num_count>max_num) return 0;
+    cell++;
+    if(*cell == '\0') return 1;
+    return 0;
+}
+
+int is_valid_constant(char *cst){
+    if (cst == NULL || *cst == '\0') return 0;
+    if (*cst == '-' || *cst == '+') cst++;
+    for(int i=0; i<strlen(cst)-1; i++){
+        if(!isdigit(cst[i])) return 0;
+    }
+    return 1;
+}
+
+int is_valid_value(char *value, int R, int C){
+    if(is_valid_cell(value, R, C)) return 1;
+    if(is_valid_constant(value)) {
+        printf("AJD");
+        return 1;
+    }
+    return 0;
+}
+
+int is_valid_range(char *range, int R, int C){
+    if(range == NULL || *range == '\0') return 0;
+    char *rptr;
+    rptr = range;
+    if(*range == '(' && *(range+strlen(range)-2) == ')'){
+        rptr++;
+        while(*rptr){
+            if(*rptr == ':') break;
+            rptr++;
+        }
+        if(!*rptr) return 0;
+
+        char cell1[100], cell2[100];
+        strncpy(cell1, range+1, rptr-range-1);
+        cell1[rptr-range-1] = '\0';
+        strncpy(cell2, rptr+1, range+strlen(range)-2-rptr-1);
+        cell2[range+strlen(range)-2-rptr-1] = '\0';
+
+        if(is_valid_cell(cell1, R, C) && is_valid_cell(cell2, R, C)){
+            if(strcmp(cell1, cell2) <= 0) return 1;
+        }
+
+    }
+    return 0;
+}
+
+int is_valid_sleep_value(char *value, int R, int C){
+    if(value == NULL || *value == '\0') return 0;
+    if(*value == '(' && *(value+strlen(value)-2) == ')'){
+        char val[100];
+        strncpy(val, value+1, strlen(value)-3);
+        val[strlen(value)-3] = '\0';
+        if(*val == '-') return 0;
+        return is_valid_value(val, R, C);
+    }
+    return 0;
+}
+
+int is_valid_formula(char *input, int R, int C){
+    char *eq;
+    eq = input;
+    while(*eq){
+        if(*eq == '=') break;
+        eq++;
+    }
+    if(!*eq) return 0;
+
+    char lhs[100], rhs[100];
+    char *rhs_ptr;
+    rhs_ptr = rhs;
+    strncpy(lhs, input, eq-input);
+    lhs[eq-input] = '\0';
+    strcpy(rhs, eq+1);
+
+    if(!is_valid_cell(lhs, R, C)) return 0;
+    if( strncmp(rhs_ptr, "MAX", strlen("MAX")) == 0 ||
+    strncmp(rhs_ptr, "MIN", strlen("MIN")) == 0 ||
+    strncmp(rhs_ptr, "AVG", strlen("AVG")) == 0 ||
+    strncmp(rhs_ptr, "SUM", strlen("SUM")) == 0 ){
+        return is_valid_range(rhs_ptr+3, R, C);
+    }
+    if( strncmp(rhs_ptr, "STDEV", strlen("STDEV")) == 0){
+        return is_valid_range(rhs_ptr+5, R, C);
+    }
+    if( strncmp(rhs_ptr, "SLEEP", strlen("SLEEP")) == 0){
+        return is_valid_sleep_value(rhs_ptr+5, R, C);
+    }
+    
+    if(*rhs_ptr == '-' || *rhs_ptr == '+'){
+        if(!isdigit(*(rhs_ptr+1))) return 0;
+        rhs_ptr++;
+    }
+    
+    char *op;
+    op = rhs_ptr;
+    while(*op){
+        if(*op == '+' || *op == '-' || *op == '*' || *op == '/') break;
+        op++;
+    }
+    if(!*op){
+        printf("SB");
+        return is_valid_value(rhs_ptr, R, C);
+    }
+    
+    char rhs1[100], rhs2[100];
+    strncpy(rhs1, rhs_ptr, op - rhs_ptr);
+    rhs1[op-rhs_ptr] = '\0';
+    strcpy(rhs2, op+1);
+
+    if(is_valid_value(rhs, R, C) && is_valid_value(rhs2, R, C)) return 1;
+
+    return 0;
+}
+
+int is_valid_input(char *input, int R, int C){
+    remove_spaces(input);
+    to_uppercase(input);
+    
+    if(strncmp(input, "W", strlen("W")) == 0){
+        if(strlen(input) == strlen("W") + 1) return 1;
+    }
+    if(strncmp(input, "S", strlen("S")) == 0){
+        if(strlen(input) == strlen("S") + 1) return 1;
+    }
+    if(strncmp(input, "A", strlen("A")) == 0){
+        if(strlen(input) == strlen("A") + 1) return 1;
+    }
+    if(strncmp(input, "D", strlen("D")) == 0){
+        if(strlen(input) == strlen("D") + 1) return 1;
+    }
+    if(strncmp(input, "Q", strlen("Q")) == 0){
+        if(strlen(input) == strlen("Q") + 1) return 1;
+    }
+    
+    if(strncmp(input, "SCROLL_TO", strlen("SCROLL_TO")) == 0){  
+        return is_valid_cell(input+9, R, C);
+    }
+    if(strncmp(input, "DISABLE_OUTPUT", strlen("DISABLE_OUTPUT")) == 0){
+        if(strlen(input) == strlen("DISABLE_OUTPUT") + 1) return 1;
+    }
+    if(strncmp(input, "ENBLE_OUTPUT", strlen("ENABLE_OUTPUT")) == 0){
+        if(strlen(input) == strlen("ENABLE_OUTPUT") + 1) return 1;
+    }
+    // printf("HD");
+    if(is_valid_formula(input, R, C) == 1) return 1;
+    
+    return 0;
+}
 
 
-enum ops {
-    CONST = 1,
-    SINGLE_CELL,
-    CELL_ADD_CONST,
-    CELL_MULT_CONST,
-    CELL_DIV_CONST,
-    CONST_DIV_CELL,
-    CONST_SUB_CELL,
-    CELL_ADD_CELL,
-    CELL_SUB_CELL,
-    CELL_MULT_CELL,
-    CELL_DIV_CELL,
-    MAX,
-    MIN,
-    SUM,
-    AVG,
-    STD_DEV,
-    SLEEP_CONST,
-    SLEEP_CELL
-};
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Assuming a valid input string
@@ -37,6 +228,15 @@ void remove_first_character(char *str){
     }
 }
 
+void insert_character_beginning(char *str, const char c){
+    if(str == NULL) return;
+    char *temp = str + strlen(str) - 1;
+    while(*temp && temp > str){
+        *temp = *(temp-1);
+        temp --;
+    }
+    *temp = c;
+}
 
 void remove_second_of_substring(char *str, const char *substr) {
     if (str == NULL || substr == NULL) return; // Handle NULL input
@@ -50,38 +250,6 @@ void remove_second_of_substring(char *str, const char *substr) {
     }
 }
 
-// void rearrange_string(char *str, const char *substr) {
-//     if (str == NULL || substr == NULL) return;
-
-//     char *pos = strstr(str, substr);
-//     if (!pos) return;
-
-//     int index = pos - str;  // Position of the substring in `str`
-//     int len_substr = strlen(substr);
-//     int len = strlen(str);
-
-//     char temp[100]; //stores rearranged string
-
-//     // Copy the part after the substring to the beginning of `temp`
-//     int temp_index = 0;
-//     for (int i = index + len_substr; i < len; i++) {
-//         if (str[i] != '\n') temp[temp_index++] = str[i];
-//     }
-
-//     // Copy the substring in place
-//     for (int i = 0; i < len_substr; i++) {
-//         temp[temp_index++] = substr[i];
-//     }
-
-//     // Copy the part before the substring to the end
-//     for (int i = 0; i < index; i++) {
-//         if(str[i] != '\n') temp[temp_index++] = str[i];
-//     }
-
-//     temp[temp_index] = '\0';
-//     strcpy(str, temp);
-// }
-
 void change_character(char *str, char old_char, char new_char) {
     if (str == NULL) return;
 
@@ -91,6 +259,14 @@ void change_character(char *str, char old_char, char new_char) {
     }
 }
 
+int num_ops(const char *rhs){
+    int count =0;
+    while(*rhs){
+        if(*rhs == '+' || *rhs == '-' || *rhs == '*' || *rhs == '/') count ++;
+        rhs ++;
+    }
+    return count;
+}
 
 //function to detect extra negative sign
 int two_op(char *rhs){
@@ -101,23 +277,51 @@ int two_op(char *rhs){
             if(strstr(rhs, "+")) change_character(rhs, '+', '-');
             else if(strstr(rhs, "-")) change_character(rhs, '-', '+');
         }
-        if (isalpha(*rhs)) return 11; //negative cell first INVALID
+        if (isalpha(*rhs)) return 11; //negative cell first
         else if (isdigit(*rhs)) return 12; //negative constant first
     }
     char *ptr1 = strstr(rhs, "*-");
     if(ptr1){
-        remove_second_of_substring(rhs, "*-");
-        if (isalpha(*(ptr1+1))) return 21; //negative cell second INVALID
+        remove_first_character(ptr1+1);
+        if (isalpha(*(ptr1+1))) return 21; //negative cell second
         else if (isdigit(*(ptr1+1))) return 22; //negative constant second
     }
     char *ptr2 = strstr(rhs, "/-");
     if(ptr2){
-        remove_second_of_substring(rhs, "/-");
-        if (isalpha(*(ptr2+1))) return 21; //negative cell second INVALID
+        remove_first_character(ptr2+1);
+        if (isalpha(*(ptr2+1))) return 21; //negative cell second
         else if (isdigit(*(ptr2+1))) return 22; //negative constant second
+    }
+    char *ptr3 = strstr(rhs, "+-");
+    if(ptr3){
+        remove_first_character(ptr3);
+        return 0;
+    }
+    char *ptr4 = strstr(rhs, "--");
+    if(ptr4){
+        remove_first_character(ptr4);
+        remove_first_character(ptr4);
+        insert_character_beginning(ptr4, '+');
+        printf("%s\n", rhs);
+        return 0;
     }
     return 0; 
 } 
+
+int three_op(char *rhs){
+    if(*rhs == '-'){
+        remove_first_character(rhs);
+        char *op_pos = strpbrk(rhs, "+-*/");
+        if (*(op_pos+1) == '-'){
+            remove_first_character(op_pos+1);
+            // printf("%s\n", rhs);
+            // printf("%c\n", *op_pos);
+            if (*op_pos == '+' || *op_pos == '-') return 31;
+            else if (*op_pos == '*' || *op_pos == '/') return 32;
+        }
+    }
+    return 0;
+}
 
 /*
 two_op: 11 means (-cell) (+, -, *, /) (   )
@@ -149,22 +353,17 @@ void parse_spreadsheet_coordinate(const char *input, int *col_num, int *row_num)
 }
 
 //break into lhs and rhs
-void remove_spaces(const char *input, char *output) {
-    while (*input) {
-        // printf("debug: %c\n", *input);
-        if (!isspace(*input)) {
-            *output = *input;
-            output++;
-        }
-        input++;
-    }
-    *output = '\0';
-}
+// void remove_spaces(char* s) {
+//     char* d = s;
+//     do {
+//         while (*d == ' ') {
+//             ++d;
+//         }
+//     } while (*s++ = *d++);
+// }
 
-void separate_lhs_rhs(const char *input, char *lhs, char *rhs) {
-    char output[100];
-    remove_spaces(input, output);
-    char *optr = output;
+void separate_lhs_rhs(char *input, char *lhs, char *rhs) {
+    char *optr = input;
     while (*optr && *optr != '=') {
         *lhs = *optr;
         lhs++;
@@ -183,7 +382,7 @@ void separate_lhs_rhs(const char *input, char *lhs, char *rhs) {
 
 //Identifying the operation
 enum ops get_operation(const char *rhs){
-    // printf("debug: %s\n", rhs);
+
     char *plus_pos = strstr(rhs, "+");
     char *minus_pos = strstr(rhs, "-");
     char *mult_pos = strstr(rhs, "*");
@@ -342,13 +541,27 @@ void separate_constant_constant(const char *rhs, int *constant1, int *constant2)
 }
 
 //Main parse function  
-int parse_input(const char *input, int *constant, int *cell_ix, int *cell_iy, int *cell_1x, int *cell_1y, int *cell_2x, int *cell_2y, enum ops *operation){
-    int flag_neg;
+int parse_input(char *input, int *constant, int *cell_ix, int *cell_iy, int *cell_1x, int *cell_1y, int *cell_2x, int *cell_2y, int *operation){
+    int flag_neg = 0;
     char lhs[100], rhs[100];
+    remove_spaces(input);
+    if(strstr(input, "scroll_to")){
+        *operation = SCROLL;
+        printf("%s\n", input);
+        parse_spreadsheet_coordinate(input+9, cell_ix, cell_iy);
+        *cell_1x = 0;
+        *cell_1y = 0;
+        *cell_2x = 0;
+        *cell_2y = 0;
+        *constant = 0;
+        return 1;
+         
+    }
     separate_lhs_rhs(input, lhs, rhs);
     parse_spreadsheet_coordinate(lhs, cell_ix, cell_iy);
-    if(two_op(rhs)) flag_neg = 1;
-    else flag_neg = 0;
+    int n = num_ops(rhs);
+    if(n==2){if(two_op(rhs)) flag_neg = 1;}
+    else if(n==3){if(three_op(rhs) == 31) flag_neg = 1;}
     enum ops op = get_operation(rhs);
     *operation = op;
     if (op == CONST){
@@ -360,7 +573,13 @@ int parse_input(const char *input, int *constant, int *cell_ix, int *cell_iy, in
             if(*op2 == '+') *constant = constant1 + constant2;
             else if(*op2 == '-') *constant = constant1 - constant2;
             else if(*op2 == '*') *constant = constant1 * constant2;
-            else if(*op2 == '/') *constant = constant1 / constant2;
+            else if(*op2 == '/') {
+                if(constant2 == 0) {
+                    *operation = ERR;
+                    *constant = 0;
+                }
+                else *constant = constant1 / constant2;
+            }
         }
         else{
             strcpy(constant_str, rhs);
@@ -441,41 +660,22 @@ int parse_input(const char *input, int *constant, int *cell_ix, int *cell_iy, in
     return 0;
 }
 
-int main() {
-    int T =100;
-    while(T--){
-        char expression[100];
-        fgets(expression, sizeof(expression), stdin);
-        int constant, cell_ix, cell_iy, cell_1x, cell_1y, cell_2x, cell_2y;
-        enum ops operation;
-        // int flag_neg;
-        parse_input(expression, &constant, &cell_ix, &cell_iy, &cell_1x, &cell_1y, &cell_2x, &cell_2y, &operation);
-        printf("Constant: %d\n", constant);
-        printf("Cell Index: %d, %d\n", cell_ix, cell_iy);
-        printf("Cell 1: %d, %d\n", cell_1x, cell_1y);
-        printf("Cell 2: %d, %d\n", cell_2x, cell_2y);
-        printf("Operation: %d\n", operation);
-        // printf("Flag: %d\n", flag_neg);
-    }
-    return 0;
-}
+// int main() {
+//     int T =100;
+//     while(T--){
+//         char expression[100];
+//         fgets(expression, sizeof(expression), stdin);
+//         int constant, cell_ix, cell_iy, cell_1x, cell_1y, cell_2x, cell_2y;
+//         enum ops operation;
+//         // int flag_neg;
+//         parse_input(expression, &constant, &cell_ix, &cell_iy, &cell_1x, &cell_1y, &cell_2x, &cell_2y, &operation);
+//         printf("Constant: %d\n", constant);
+//         printf("Cell Index: %d, %d\n", cell_ix, cell_iy);
+//         printf("Cell 1: %d, %d\n", cell_1x, cell_1y);
+//         printf("Cell 2: %d, %d\n", cell_2x, cell_2y);
+//         printf("Operation: %d\n", operation);
+//     }
 
-//     CONST = 1,
-//     SINGLE_CELL =2 ,
-//     CELL_ADD_CONST = 3,
-//     CELL_MULT_CONST = 4,
-//     CELL_DIV_CONST =5,
-//     CONST_DIV_CELL = 6,
-//     CONST_SUB_CELL = 7,
-//     CELL_ADD_CELL = 8,
-//     CELL_SUB_CELL = 9,
-//     CELL_MULT_CELL = 10,
-//     CELL_DIV_CELL = 11,
-//     MAX = 12,
-//     MIN = 13,
-//     SUM = 14,
-//     AVG = 15,
-//     STD_DEV = 16,
-//     SLEEP = 17
+
 
  
